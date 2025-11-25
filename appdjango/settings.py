3c8ff635a33aa507550cbbf6f6400a6923a5cc55
isa-alt2000 <isa.alt2000@gmail.com>
2025-11-24 18:04:05 -0600

    Dockerización simple del proyecto. Se elimina el setup de postgres

--- appdjango/settings.py
+++ appdjango/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-y3wv-ktz9@i**)g+&_h@*r%s0tl+%m$)uj)o!(i@#@l=(4@c(8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
